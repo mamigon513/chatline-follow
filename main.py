27bf1112944677995a2af87528ce65c9a8966aad
Marina Amigon <mamigon@nd.edu>
2025-04-12 19:16:31 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-mag = 0
-error = 0
 
 def on_forever():
-    global mag, error
     #error = CutebotPro.get_offset()
-    magY = input.magnetic_force(Dimension.Y)
-    magX = input.magnetic_force(Dimension.X)
-    magZ = input.magnetic_force(Dimension.Z) 
-    serial.write_line("" + str(magX) + ", " + str(magY) + ", " + str(magZ))
+    #magY = input.magnetic_force(Dimension.Y)
+    #magX = input.magnetic_force(Dimension.X)
+    #magZ = input.magnetic_force(Dimension.Z) 
+    #serial.write_line("" + str(magX) + ", " + str(magY) + ", " + str(magZ))
+    #strength = Math.pow((magX*magX + magY*magY + magZ*magZ), .5)
+    #serial.write_line(str(strength))
+    
+    sonar = CutebotPro.ultrasonic(SonarUnit.CENTIMETERS)
+    if sonar < 5:
+        CutebotPro.single_headlights(CutebotProRGBLight.RGBL, 0, 255, 0)
+        #serial.write_line(str(sonar))
+    else:
+        CutebotPro.turn_off_all_headlights()
+    
+    
+
     #CutebotPro.color_light(CutebotProRGBLight.RGBL, 0x00ff00)
     basic.pause(300)
-    CutebotPro.turn_off_all_headlights()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,13 +1,19 @@
-let mag = 0
-let error = 0
 basic.forever(function on_forever() {
-    
     // error = CutebotPro.get_offset()
-    let magY = input.magneticForce(Dimension.Y)
-    let magX = input.magneticForce(Dimension.X)
-    let magZ = input.magneticForce(Dimension.Z)
-    serial.writeLine("" + ("" + magX) + ", " + ("" + magY) + ", " + ("" + magZ))
+    // magY = input.magnetic_force(Dimension.Y)
+    // magX = input.magnetic_force(Dimension.X)
+    // magZ = input.magnetic_force(Dimension.Z) 
+    // serial.write_line("" + str(magX) + ", " + str(magY) + ", " + str(magZ))
+    // strength = Math.pow((magX*magX + magY*magY + magZ*magZ), .5)
+    // serial.write_line(str(strength))
+    let sonar = CutebotPro.ultrasonic(SonarUnit.Centimeters)
+    if (sonar < 5) {
+        CutebotPro.singleHeadlights(CutebotProRGBLight.RGBL, 0, 255, 0)
+    } else {
+        // serial.write_line(str(sonar))
+        CutebotPro.turnOffAllHeadlights()
+    }
+    
     // CutebotPro.color_light(CutebotProRGBLight.RGBL, 0x00ff00)
     basic.pause(300)
-    CutebotPro.turnOffAllHeadlights()
 })
